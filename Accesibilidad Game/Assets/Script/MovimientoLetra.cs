0d6aae3e5c0fadfc5a04716215eedd22d6994ecf
Camilo <camilogomezr25@gmail.com>
2023-11-02 12:42:52 -0500

    Crear el movimiento en el juego del contraste

---
+++ Accesibilidad Game/Assets/Script/MovimientoLetra.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoLetra : MonoBehaviour
+{
+    bool derecha = false;
+    bool izquierda = false;
+
+    public Rigidbody2D rb; //Agarra el componente del Rigibody del objeto
+    public float fuerzaMovimiento; //Fuerza de movimiento
+
+    public void pulsarIzquierda()
+    {
+        izquierda = true;
+    }
+    public void pulsarDerecha()
+    {
+        derecha = true;
+    }
+    public void soltarIzquierda()
+    {
+        izquierda = false;
+    }
+    public void soltarDerecha()
+    {
+        derecha = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (izquierda)
+        {
+            rb.AddForce(new Vector2(-fuerzaMovimiento, 0) * Time.deltaTime);
+        }
+        if (derecha)
+        {
+            rb.AddForce(new Vector2(fuerzaMovimiento, 0) * Time.deltaTime);
+        }
+    }
+}
